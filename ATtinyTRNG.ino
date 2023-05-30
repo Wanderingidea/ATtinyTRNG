dcffac16199f564ff92a9c3ff5a6f556479a80b7
Wanderingidea <42114791+Wanderingidea@users.noreply.github.com>
2023-05-30 19:29:15 +0200

    Update ATtinyTRNG.ino

--- ATtinyTRNG.ino
+++ ATtinyTRNG.ino
@@ -27,39 +27,39 @@
  * cat /dev/ttyACM0 > test.bin:
  *
  * Me:
-    minimum entropy: 0.99994680 bits per bit
+ * minimum entropy: 0.99994680 bits per bit
  *
  * Ent:
-    Entropy = 1.000000 bits per bit.
-
-    Optimum compression would reduce the size
-    of this 204722968 bit file by 0 percent.
-
-    Chi square distribution for 204722968 samples is 0.28, and randomly
-    would exceed this value 59.77 percent of the times.
-
-    Arithmetic mean value of data bits is 0.5000 (0.5 = random).
-    Monte Carlo value for Pi is 3.141752955 (error 0.01 percent).
-    Serial correlation coefficient is -0.000026 (totally uncorrelated = 0.0).
+ * Entropy = 1.000000 bits per bit.
+ *
+ * Optimum compression would reduce the size
+ * of this 204722968 bit file by 0 percent.
+ *
+ * Chi square distribution for 204722968 samples is 0.28, and randomly
+ * would exceed this value 59.77 percent of the times.
+ *
+ * Arithmetic mean value of data bits is 0.5000 (0.5 = random).
+ * Monte Carlo value for Pi is 3.141752955 (error 0.01 percent).
+ * Serial correlation coefficient is -0.000026 (totally uncorrelated = 0.0).
  *
  * Rngtest:
-    rngtest 6.16
-    Copyright (c) 2004 by Henrique de Moraes Holschuh
-    This is free software; see the source for copying conditions.  There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-    rngtest: starting FIPS tests...
-    rngtest: entropy source drained
-    rngtest: bits received from input: 204722968
-    rngtest: FIPS 140-2 successes: 10235
-    rngtest: FIPS 140-2 failures: 1
-    rngtest: FIPS 140-2(2001-10-10) Monobit: 0
-    rngtest: FIPS 140-2(2001-10-10) Poker: 0
-    rngtest: FIPS 140-2(2001-10-10) Runs: 0
-    rngtest: FIPS 140-2(2001-10-10) Long run: 1
-    rngtest: FIPS 140-2(2001-10-10) Continuous run: 0
-    rngtest: input channel speed: (min=280.492; avg=4261.869; max=6357.829)Mibits/s
-    rngtest: FIPS tests speed: (min=43.153; avg=113.487; max=123.854)Mibits/s
-    rngtest: Program run time: 1768244 microseconds
+ * rngtest 6.16
+ * Copyright (c) 2004 by Henrique de Moraes Holschuh
+ * This is free software; see the source for copying conditions.  There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * rngtest: starting FIPS tests...
+ * rngtest: entropy source drained
+ * rngtest: bits received from input: 204722968
+ * rngtest: FIPS 140-2 successes: 10235
+ * rngtest: FIPS 140-2 failures: 1
+ * rngtest: FIPS 140-2(2001-10-10) Monobit: 0
+ * rngtest: FIPS 140-2(2001-10-10) Poker: 0
+ * rngtest: FIPS 140-2(2001-10-10) Runs: 0
+ * rngtest: FIPS 140-2(2001-10-10) Long run: 1
+ * rngtest: FIPS 140-2(2001-10-10) Continuous run: 0
+ * rngtest: input channel speed: (min=280.492; avg=4261.869; max=6357.829)Mibits/s
+ * rngtest: FIPS tests speed: (min=43.153; avg=113.487; max=123.854)Mibits/s
+ * rngtest: Program run time: 1768244 microseconds
  *
  * Practical implementation in Linux:
  * in crontab @reboot:
